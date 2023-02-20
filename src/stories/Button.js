a6d0a395eeaf14a65a74747fad47e25df2a76e08
yeomdogyeong <kangs19951@gmail.com>
2023-02-20 13:36:14 +0900

    수정완료

--- src/stories/Button.js
+++ src/stories/Button.js
@@ -2,10 +2,10 @@ import React from "react";
 import styled from "styled-components";
 
 //styledcomponent로 조건 만들어주기
-const StyledButton = styled.button` 
-background: ${(props) => props.color || "white"} 
-width: ${(props) => (props.size === "big" ? "200px" : "100px")};
-height: ${(props) => (props.size === "big" ? "80px" : "40px")};
+const StyledButton = styled.button`
+  background: ${(props) => props.color || "white"};
+  width: ${(props) => (props.size === "big" ? "200px" : "100px")};
+  height: ${(props) => (props.size === "big" ? "80px" : "40px")};
 `;
 
 //버튼 안의 props들 설정해주기
